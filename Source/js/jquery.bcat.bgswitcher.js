9c018fdb55fe0c0fdff95730928fa3e0bec6f834
bcat <me@bcat.eu>
2013-10-01 16:57:22 +0200

    Added new option to show the navigation links.

--- Source/js/jquery.bcat.bgswitcher.js
+++ Source/js/jquery.bcat.bgswitcher.js
@@ -1,6 +1,6 @@
 /**
  * bcat BG Switcher - unobtrusive background image switcher
- * @version 1.1.0
+ * @version 1.2.0
  * @jQuery version 1.2+
  * @author Yuriy Davats http://www.bcat.eu
  * @copyright Yuriy Davats
@@ -16,12 +16,13 @@
         startIndex: 0, // first image loaded
         timeout: 12000, // time between image changes
         alt: 'Picture', // alt for consistency
-        speed: 1000 // animation speed        
+        speed: 1000, // animation speed        
+        links: false // generate a link for each image
     };
 
     // Plugin constructor
     function Plugin(element, options) {
-                
+
         this._defaults = defaults;
         this._name = pluginName;
 
@@ -29,7 +30,7 @@
     }
 
     Plugin.prototype = {
-        init: function(element, options) {  
+        init: function(element, options) {
             var instance = {};
             instance.currentIndex = options.startIndex;
             instance.currentImage = this.preloadImage(element, options, instance.currentIndex);
@@ -41,15 +42,23 @@
                 instance.currentImage.fadeIn(options.speed);
                 instance.currentIndex++;
                 if (options.urls[instance.currentIndex]) {
+                    if (options.links) {
+                        var loaderDiv = $('<div />').attr({
+                            'id': element.id + '-loader',
+                            'style': 'display: none;'
+                        });
+                        loaderDiv.appendTo(element);                        
+                        that.generateLinks(element, options, instance);
+                    }
                     that.runSlideShow(element, options, instance);
-                }                
+                }
             });
         },
         runSlideShow: function(element, options, instance) {
             // fix scope
             var that = this;
             // update image periodically
-            setInterval(function() {
+            instance.intervalId = setInterval(function() {
                 that.updateImage(element, options, instance);
             }, options.timeout);
         },
@@ -60,8 +69,8 @@
             if (!options.urls[instance.currentIndex]) {
                 instance.currentIndex = 0;
             }
-                        
-            var nextImage = $('#'+ element.id + instance.currentIndex);
+
+            var nextImage = $('#' + element.id + instance.currentIndex);
             if (nextImage.length) {
                 // image found in DOM, changing visibility                
                 instance.currentImage.fadeOut(options.speed);
@@ -71,6 +80,9 @@
                 nextImage = this.preloadImage(element, options, instance.currentIndex);
                 this.swapPreloadedImages(instance.currentImage, nextImage, element, options);
             }
+            if (options.links) {
+                this.setActiveLink(element, options, instance);
+            }
             instance.currentImage = nextImage;
             instance.currentIndex++;
         },
@@ -79,7 +91,7 @@
             if (!style) {
                 style = 'display: none;';
             }
-                                                
+
             var img = $('<img />');
             img.attr({
                 'id': element.id + index,
@@ -89,23 +101,115 @@
             });
             return img;
         },
-        swapPreloadedImages: function(currentImage, nextImage, element, options) {            
+        swapPreloadedImages: function(currentImage, nextImage, element, options, showLoader) {
             // swap images on load
+
+            if (showLoader) {
+                // show loader
+                $(element).addClass('loading');
+            }
+
             nextImage.load(function() {
+                if (showLoader) {
+                    // hide loader
+                    $(element).removeClass('loading');
+                }
                 nextImage.appendTo(element);
                 currentImage.fadeOut(options.speed);
                 nextImage.fadeIn(options.speed);
             });
+        },
+        generateLinks: function(element, options, instance) {
+            // generate image links on load
+            instance.linkParent = $('<div />');
+            instance.linkParent.attr({
+                'id': element.id + '-navigation',
+                'style': 'display: none;'
+            });
+
+            // fix scope
+            var that = this;
+
+            $.each(options.urls, function(index, value) {
+                var link = $('<a />');
+                var linkClass = '';
+                if (index === options.startIndex) {
+                    linkClass = 'active';
+                }
+                link.attr({
+                    'id': element.id + '-link' + index,
+                    'href': '#',
+                    'class': linkClass
+                });
+                link.click(function(event) {
+                    event.preventDefault();
+                    that.switchImageTo(element, options, instance, index);
+                });
+                instance.linkParent.append(link);
+            });
+
+            instance.linkParent.insertAfter(element);
+            instance.linkParent.fadeIn(options.speed);
+
+        },
+        switchImageTo: function(element, options, instance, index) {
+            // switch to the given image using array index and reset slideshow
+            
+            if (!options.urls[index]) {
+                console.log('can not switch to a non-existent element');
+                return;
+            }
+
+            var nextImage = $('#' + element.id + index);
+
+            // prevent action on active image
+            if (nextImage.attr('id') !== instance.currentImage.attr('id')) {
+                
+                instance.currentIndex = index;
+
+                // stop slide show
+                clearInterval(instance.intervalId);
+
+                if (nextImage.length) {
+                    // image found in DOM, changing visibility                
+                    instance.currentImage.fadeOut(options.speed);
+                    nextImage.fadeIn(options.speed);
+                } else {
+                    // image was not loaded yet, loading and showing it                
+                    nextImage = this.preloadImage(element, options, instance.currentIndex);
+                    this.swapPreloadedImages(instance.currentImage, nextImage, element, options, true);
+                }
+
+                this.setActiveLink(element, options, instance);
+                instance.currentImage = nextImage;
+                instance.currentIndex++;
+
+                // fix scope
+                var that = this;
+                // run slideshow again
+                instance.intervalId = setInterval(function() {
+                    that.updateImage(element, options, instance);
+                }, options.timeout);
+
+            }
+
+        },
+        setActiveLink: function(element, options, instance) {
+            // set active class to the currently active link
+            if (instance.linkParent.length) {
+                instance.linkParent.find('a').removeClass('active');
+                instance.linkParent.find('a#' + element.id + '-link' + instance.currentIndex).addClass('active');
+            }
         }
     };
 
     // Plugin wrapper preventing against multiple instantiations
     $.fn[pluginName] = function(options) {
         return this.each(function() {
-            if (!$.data(this, "plugin_" + pluginName)) {                
-                var localOptions = $.extend({}, defaults, options);                
-                $.data(this, "plugin_" + pluginName, new Plugin(this, localOptions));                
-                
+            if (!$.data(this, "plugin_" + pluginName)) {
+                var localOptions = $.extend({}, defaults, options);
+                $.data(this, "plugin_" + pluginName, new Plugin(this, localOptions));
+
             }
         });
     };
